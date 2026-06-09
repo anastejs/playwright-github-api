49dda8dbffb64cc110a830ffe728c5a11ec901b3
Anastasia <anastejs.cherednichenko@gmail.com>
2026-06-09 22:18:44 +0200

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from creds import *
+from playwright.sync_api import *
+
+
+@pytest.fixture(scope="session")                            # session - создать один раз, использовать для всех тестов и закрыть в конце
+def api_context(playwright: Playwright):
+    headers = {
+        "Accept": "application/vnd.github.v3+json",         # хотим получать ответы в формате GitHub API v3
+        "Authorization": f"token {GITHUB_ACCESS_TOKEN}"     # acces token (аналог логина)
+    }
+    context = playwright.request.new_context(               # cоздание API клиента
+        base_url="https://api.github.com/",
+        extra_http_headers=headers,
+    )
+    yield context                                           # передаём клиент тестам
+    context.dispose()                                       # закрываем API клиент
+
+# Test hook для создания тестового репозитория 
+@pytest.fixture(autouse=True, scope="session")              # autouse - не нужно явно вызывать fixture, pytest запустит сам, session - запустится один раз перед всеми тестами
+def create_test_repository(api_context: APIRequestContext):
+    # Create the repo
+    post_response = api_context.post(                       # отправляем POST запрос, GitHub получает JSON и создаёт репозиторий
+        "/user/repos",
+        data={"name": GITHUB_REPO}
+    )
+    assert post_response.ok                                 # проверка - создался ли репозиторий? (status = 201 Created)
+
+    yield
+
+    # Delete the repo
+    delete_response = api_context.delete(f"/repos/{GITHUB_USER}/{GITHUB_REPO}")    # отправляем DELETE запрос
+    assert delete_response.ok                               # проверка - удалился ли репозиторий? (status = 204 No Content)
---
+++ tests/test_github_issue.py
@@ -0,0 +1,44 @@
+from creds import *
+from playwright.sync_api import APIRequestContext, Page
+
+
+# тест 1 - создаем новое issue в репозитории
+def test_create_issue(api_context: APIRequestContext):        # получаем API-клиент
+    issue_data = {
+        "title": "[Bug] That failed",
+        "body": "When running this, that failed with error.",
+        "assignees": ["anastejs"],
+    }
+
+    issue = api_context.post(                                 # отправляем POST запрос - создаем новое issue
+        f"/repos/{GITHUB_USER}/{GITHUB_REPO}/issues",
+        data=issue_data,
+    )
+    assert issue.ok                                           # проверка - создалось ли issue? (status = 201 Created)
+
+
+# тест 2 - проверка (скриншот) существования нового issue в нашем репозитории
+def test_issue_page(page: Page):                              # получаем браузерную вкладку
+    page.goto(f"https://github.com/{GITHUB_USER}/{GITHUB_REPO}/issues")
+    page.screenshot(path="test-results/issues-page.jpg", full_page=True)
+
+
+# тест 3 - поиск созданного Issue во вкладке "Issues" в нашем репозитории
+def test_new_issue_created(api_context: APIRequestContext):   # получаем API-клиент
+    all_issues = api_context.get(f"/repos/{GITHUB_USER}/{GITHUB_REPO}/issues")   # отправляем GET запрос - получаем список всех issues
+    assert all_issues.ok                                                         # проверка ответа 
+
+    new_issue = [                                             # находим нужный issue по его названию
+        issue 
+        for issue in all_issues.json()
+        if issue["title"] == "[Bug] That failed"
+    ][0]
+
+    # print("\nAll issues:")
+    # for issue in all_issues.json():
+    #     print(issue["title"])
+
+    # проверка - создан именно тот Issue, который мы создавали
+    assert new_issue["body"] == "When running this, that failed with error."     
+
+# pytest -v -s
