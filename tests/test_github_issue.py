86b2b8413a3694f2c03271cbb66586d22d3085ac
Anastasia <anastejs.cherednichenko@gmail.com>
2026-06-09 22:33:06 +0200

    Update creds

--- tests/test_github_issue.py
+++ tests/test_github_issue.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-GITHUB_USER = os.getenv("GITHUB_USER")
-GITHUB_REPO = os.getenv("GITHUB_REPO")
-GITHUB_ACCESS_TOKEN= os.getenv("GITHUB_ACCESS_TOKEN")
+GITHUB_USER = os.getenv("GIT_USER")
+GITHUB_REPO = os.getenv("GIT_REPO")
+GITHUB_ACCESS_TOKEN= os.getenv("GIT_ACCESS_TOKEN")
 
 # тест 1 - создаем новое issue в репозитории
 def test_create_issue(api_context: APIRequestContext):        # получаем API-клиент
